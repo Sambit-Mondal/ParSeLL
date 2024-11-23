af53237a6a7266f7e2ec8b00e29a4cd445e1e1c6
AdarshRout <likun2432@gmail.com>
2024-11-23 23:48:11 +0530

    minor tweaks

--- my-chat-app/src/lib/aws-config.ts
+++ my-chat-app/src/lib/aws-config.ts
@@ -6,7 +6,7 @@ if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
 }
 
 const translateClient = new TranslateClient({
-  region: "ap-south-1", // Replace with your AWS region
+  region: process.env.AWS_REGION, // Replace with your AWS region
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
